57a17e3706ed9d7ac83fa3905c863aed1387de19
VictorNarciso93 <victornarciso8370@gmail.com>
2025-03-13 22:23:17 -0300

    Acrescentando verificação do retorno da api

--- app.data.py
+++ app.data.py
@@ -14,3 +14,17 @@ films_endpoint = "films/"
 
 # REQUISIÇÃO GET
 response = requests.get(base_url + films_endpoint)
+
+
+# VERIFICANDO RETORNO DA REQUISIÇÃO
+if response.status_code == 200:
+    data = response.json()
+    for film in data['results']:
+        print(f"Title: {film['title']}")
+        print(f"Episode: {film['episode']}")
+        print(f"Director: {film['director']}")
+        print(f"Producer: {film['producer']}")
+        print(f"Release Date: {film['release_date']}")
+        print("-" * 30)
+else:
+    print(f"Erro: {response.status_code}")

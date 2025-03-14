df40da99c33d264d38cff1db68c0436d10906dd1
VictorNarciso93 <victornarciso8370@gmail.com>
2025-03-13 22:04:41 -0300

    Adicionando url e efetuando requisicao GET

--- app.data.py
+++ app.data.py
@@ -3,3 +3,14 @@ import time
 import os
 import pandas as import pd
 
+
+# URL PÚBLICA DE STAR WARS
+base_url = "https://swapi.dev/api/"
+
+
+# ENDPOINT PARA OBTER AS INFORMAÇÕES
+films_endpoint = "films/"
+
+
+# REQUISIÇÃO GET
+response = requests.get(base_url + films_endpoint)
